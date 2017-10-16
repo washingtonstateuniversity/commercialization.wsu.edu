253cecbaf39eebc60ddb875038168bade621cc44
Phil Cable <pcable@wsu.edu>
2017-10-16 16:24:28 -0700

    Provide a theme version for use in cache busting

--- functions.php
+++ functions.php
@@ -1,6 +1,18 @@
 <?php
 
+add_filter( 'spine_child_theme_version', 'commercialization_theme_version' );
 add_action( 'init', 'commercialization_register_footer_menu' );
+
+/**
+ * Provides a theme version for use in cache busting.
+ *
+ * @since 0.0.1
+ *
+ * @return string
+ */
+function commercialization_theme_version() {
+	return '0.0.1';
+}
 
 /**
  * Registers the menu location for the site footer.
